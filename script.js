a87e42b1baebdb600fcdaf0bc0550db576d9bf21
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-05-14 22:54:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 //your JS code here. If required.
 document.addEventListener('DOMContentLoaded', function() {
-    // Retrieve the status paragraph element
     const statusElement = document.getElementById('status');
-    
-    // Retrieve the enter button
     const enterBtn = document.getElementById('enterBtn');
-
-    // Add click event listener to the enter button
-    enterBtn.addEventListener('click', function() {
-        // Change the text of the status paragraph
+ enterBtn.addEventListener('click', function() {
         statusElement.innerHTML = '<h1>Entered Metaverse</h1>';
     });
 });
